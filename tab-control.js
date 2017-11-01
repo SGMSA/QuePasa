142112764fed4f40fbbbc2adac9dd7171cac7e06
Sam <sgmsa@hotmail.co.uk>
2017-11-01 13:48:04 +0000

    Poorly attempting to add JavaScript

--- tab-control.js
+++ tab-control.js
@@ -1,16 +1,33 @@
-var display = 1;
-var hidden = 0;
-
-
-
-function tabControl() {
+function main() {
     
-            
-            if (hidden == 0){
+    tabControl();
+    
+    var x = document.getElementById("tab1");
+    var y = document.getElementById("tab2");
+    var z = document.getElementById("tab3");
+
+    x.addEventListener("click", tabControl);
+    y.addEventListener("click", tabControl);
+    z.addEventListener("click", tabControl);
+
+    function tabControl() {
+        if (x == "click") {
+            document.getElementById("section1").style.visibility = "visible";
+        }
+        else {
             document.getElementById("section1").style.visibility = "hidden";
+        }
+        if (y == "click") {
+            document.getElementById("section2").style.visibility = "visible";
+        }
+        else {
+            document.getElementById("section2").style.visibility = "hidden";
+        }
+        if (z == "click") {
+            document.getElementById("section3").style.visibility = "visible";
+        }
+        else {
             document.getElementById("section3").style.visibility = "hidden";
-            }
-            else (display == 1){
-            document.getElementById("section2").style.visibility = "visible";   
-            }
+        }
+    }
 }
